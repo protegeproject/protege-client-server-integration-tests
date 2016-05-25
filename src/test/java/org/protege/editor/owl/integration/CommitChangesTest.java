f6b981408eacc7be2db5a947939f33e5589e3b63
johardi <josef.hardi@stanford.edu>
2016-05-25 15:05:34 -0700

    Add a commit test that contains axiom deletions

--- src/test/java/org/protege/editor/owl/integration/CommitChangesTest.java
+++ src/test/java/org/protege/editor/owl/integration/CommitChangesTest.java
@@ -23,12 +23,19 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.semanticweb.owlapi.model.AddAxiom;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLAnnotationSubject;
+import org.semanticweb.owlapi.model.OWLAnnotationValue;
+import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import com.google.common.collect.Lists;
 
@@ -49,6 +56,7 @@ public class CommitChangesTest extends BaseTest {
 
     private static final OWLClass DOMAIN_CONCEPT = Class(IRI(ONTOLOGY_ID, "DomainConcept"));
     private static final OWLClass CUSTOMER = Class(IRI(ONTOLOGY_ID, "Customer"));
+    private static final OWLClass MEAT_TOPPING = Class(IRI(ONTOLOGY_ID, "MeatTopping"));
 
     private ProjectId projectId;
 
@@ -134,6 +142,66 @@ public class CommitChangesTest extends BaseTest {
         assertThat(changeHistoryFromServer.getRevisions().size(), is(2));
         assertThat(changeHistoryFromServer.getChangesForRevision(R1).size(), is(945));
         assertThat(changeHistoryFromServer.getChangesForRevision(R2).size(), is(2));
+    }
+
+    @Test
+    public void shouldCommitDeletion() throws Exception {
+        VersionedOWLOntology vont = openProjectAsAdmin();
+        OWLOntology workingOntology = vont.getOntology();
+        
+        /*
+         * Simulates user edits over a working ontology (remove a class and its references)
+         */
+        Set<OWLAxiom> axiomsToRemove = new HashSet<>();
+        for (OWLAxiom ax : workingOntology.getAxioms()) {
+            if (ax.getSignature().contains(MEAT_TOPPING)) {
+                axiomsToRemove.add(ax);
+            }
+            if (ax instanceof OWLAnnotationAssertionAxiom) {
+                OWLAnnotationAssertionAxiom asa = (OWLAnnotationAssertionAxiom) ax;
+                OWLAnnotationSubject subject = asa.getSubject();
+                if (subject instanceof IRI && subject.equals(MEAT_TOPPING.getIRI())) {
+                    axiomsToRemove.add(ax);
+                }
+                OWLAnnotationValue value = asa.getValue();
+                if (value instanceof IRI && value.equals(MEAT_TOPPING.getIRI())) {
+                    axiomsToRemove.add(ax);
+                }
+            }
+        }
+        owlManager.removeAxioms(workingOntology, axiomsToRemove);
+        List<OWLOntologyChange> changes = ClientUtils.getUncommittedChanges(vont);
+        
+        RevisionMetadata metadata = new RevisionMetadata(
+                getAdmin().getUserInfo().getId(),
+                getAdmin().getUserInfo().getName(),
+                getAdmin().getUserInfo().getEmailAddress(),
+                "Remove MeatTopping and its references");
+        CommitBundle commitBundle = new CommitBundleImpl(vont.getHeadRevision(), new Commit(metadata, changes));
+        ChangeHistory approvedChanges = getAdmin().commit(projectId, commitBundle);
+        vont.update(approvedChanges);
+
+        ChangeHistory changeHistoryFromClient = vont.getChangeHistory();
+
+        // Assert the local change history
+        assertThat("The local change history should not be empty", !changeHistoryFromClient.isEmpty());
+        assertThat(changeHistoryFromClient.getBaseRevision(), is(R0));
+        assertThat(changeHistoryFromClient.getHeadRevision(), is(R2));
+        assertThat(changeHistoryFromClient.getMetadata().size(), is(2));
+        assertThat(changeHistoryFromClient.getRevisions().size(), is(2));
+        assertThat(changeHistoryFromClient.getChangesForRevision(R1).size(), is(945));
+        assertThat(changeHistoryFromClient.getChangesForRevision(R2).size(), is(16));
+        
+        ChangeHistory changeHistoryFromServer = ChangeUtils.getAllChanges(vont.getServerDocument());
+        
+        // Assert the remote change history
+        assertThat("The remote change history should not be empty", !changeHistoryFromServer.isEmpty());
+        assertThat(changeHistoryFromServer.getBaseRevision(), is(R0));
+        assertThat(changeHistoryFromServer.getHeadRevision(), is(R2));
+        assertThat(changeHistoryFromServer.getMetadata().size(), is(2));
+        assertThat(changeHistoryFromServer.getRevisions().size(), is(2));
+        assertThat(changeHistoryFromServer.getChangesForRevision(R1).size(), is(945));
+        assertThat(changeHistoryFromServer.getChangesForRevision(R2).size(), is(16));
     }
 
     @Test(expected=ClientRequestException.class)
