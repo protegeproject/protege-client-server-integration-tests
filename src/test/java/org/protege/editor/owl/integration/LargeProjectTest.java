482b533dab58687d658515060c1a65ca7393ca3f
Bob Dionne <dionne@dionne-associates.com>
2016-06-01 10:11:07 -0400

    Add open test to large ontology

--- src/test/java/org/protege/editor/owl/integration/LargeProjectTest.java
+++ src/test/java/org/protege/editor/owl/integration/LargeProjectTest.java
@@ -11,18 +11,23 @@ import java.util.Optional;
 
 import org.junit.After;
 import org.junit.Test;
+import org.protege.editor.owl.client.api.Client;
 import org.protege.editor.owl.client.util.ChangeUtils;
 import org.protege.editor.owl.client.util.ClientUtils;
+import org.protege.editor.owl.integration.BaseTest.PizzaOntology;
 import org.protege.editor.owl.server.api.CommitBundle;
 import org.protege.editor.owl.server.policy.CommitBundleImpl;
 import org.protege.editor.owl.server.versioning.Commit;
 import org.protege.editor.owl.server.versioning.api.ChangeHistory;
 import org.protege.editor.owl.server.versioning.api.ServerDocument;
+import org.protege.editor.owl.server.versioning.api.VersionedOWLOntology;
+import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
 
 import edu.stanford.protege.metaproject.api.Description;
 import edu.stanford.protege.metaproject.api.Name;
+import edu.stanford.protege.metaproject.api.PlainPassword;
 import edu.stanford.protege.metaproject.api.ProjectId;
 import edu.stanford.protege.metaproject.api.ProjectOptions;
 import edu.stanford.protege.metaproject.api.UserId;
@@ -35,7 +40,7 @@ public class LargeProjectTest extends BaseTest {
         /*
          * [GUI] The input project properties
          */
-        projectId = f.getProjectId("BiomedGt");
+        projectId = f.getProjectId("BiomedGT");
         Name projectName = f.getName("NCI Thesaurus" );
         Description description = f.getDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit");
         UserId owner = f.getUserId("root");
@@ -44,7 +49,8 @@ public class LargeProjectTest extends BaseTest {
         /*
          * [GUI] The input target ontology
          */
-        OWLOntology ontology = owlManager.loadOntologyFromOntologyDocument(LargeOntology.getResource());
+        OWLOntology ontology = OWLManager.createOWLOntologyManager().loadOntologyFromOntologyDocument(LargeOntology.getResource());
+
 
         /*
          * [NewProjectAction] Compute the initial commit from the input ontology
@@ -74,12 +80,41 @@ public class LargeProjectTest extends BaseTest {
         assertThat(remoteChangeHistory.getHeadRevision(), is(R1));
         assertThat(remoteChangeHistory.getMetadata().size(), is(1));
         assertThat(remoteChangeHistory.getRevisions().size(), is(1));
-        assertThat(remoteChangeHistory.getChangesForRevision(R1).size(), is(945));
+        //assertThat(remoteChangeHistory.getChangesForRevision(R1).size(), is(945));
+    }
+    
+    @After
+    public void shouldDownloadRemoteChanges() throws Exception {
+        /*
+         * Login as Guest
+         */
+        UserId guestId = f.getUserId("guest");
+        PlainPassword guestPassword = f.getPlainPassword("guestpwd");
+        Client guest = login(guestId, guestPassword);
+        
+        ServerDocument serverDocument = guest.openProject(projectId);
+        VersionedOWLOntology vont = ClientUtils.buildVersionedOntology(serverDocument, owlManager);
+        ChangeHistory changeHistoryFromClient = vont.getChangeHistory();
+        
+        // Assert the remote change history
+        assertThat("The local change history should not be empty", !changeHistoryFromClient.isEmpty());
+        assertThat(changeHistoryFromClient.getBaseRevision(), is(R0));
+        assertThat(changeHistoryFromClient.getHeadRevision(), is(R1));
+        assertThat(changeHistoryFromClient.getMetadata().size(), is(1));
+        assertThat(changeHistoryFromClient.getRevisions().size(), is(1));
+        //assertThat(changeHistoryFromClient.getChangesForRevision(R1).size(), is(945));
+        
+        ChangeHistory changeHistoryFromServer = ChangeUtils.getAllChanges(vont.getServerDocument());
+        
+        // Assert the remote change history
+        assertThat("The remote change history should not be empty", !changeHistoryFromServer.isEmpty());
+        assertThat(changeHistoryFromServer.getBaseRevision(), is(R0));
+        assertThat(changeHistoryFromServer.getHeadRevision(), is(R1));
+        assertThat(changeHistoryFromServer.getMetadata().size(), is(1));
+        assertThat(changeHistoryFromServer.getRevisions().size(), is(1));
+        //assertThat(changeHistoryFromServer.getChangesForRevision(R1).size(), is(945));
     }
 
-    @After
-    public void removeProject() throws Exception {
-        getAdmin().deleteProject(projectId, true);
-    }
+    
 
 }
